e73b6639dadb52c1531474ae4100413648c8377b
Nathan Button <nate601@live.com>
2019-06-20 13:00:43 -0500

    Prettify formatting of IsbnForm

--- lib/main.dart
+++ lib/main.dart
@@ -46,43 +46,57 @@ class IsbnForm extends StatefulWidget {
 
 class _IsbnFormState extends State<IsbnForm> {
   final _formKey = GlobalKey<FormState>();
-
+  final _isbnController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                labelText: "ISBN:",
-                filled: true,
-                border: OutlineInputBorder(),
-                icon: Icon(Icons.book),
-              ),
-              keyboardType: TextInputType.number,
-              validator: (String value) =>
-                  value.length == 10 || value.length == 13
-                      ? null
-                      : "Enter a valid ISBN",
-              autovalidate: true,
+      child: Center(
+        child: Card(
+          elevation: 2.0,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "ISBN:",
+                      filled: true,
+                      border: OutlineInputBorder(),
+                      icon: Icon(Icons.book),
+                    ),
+                    keyboardType: TextInputType.number,
+                    validator: isbnValidate,
+                    autovalidate: true,
+                    controller: _isbnController,
+                  ),
+                ),
+                RaisedButton(
+                  child: Text(
+                    "Search",
+                  ),
+                  onPressed: isbnFormSubmit,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 100,
+                  ),
+                ),
+              ],
             ),
           ),
-          RaisedButton(
-            child: Text("Search"),
-            onPressed: isbnFormSubmit,
-            padding: EdgeInsets.symmetric(horizontal: 100),
-          )
-        ],
+        ),
       ),
     );
   }
 
+  String isbnValidate(String value) =>
+      value.length == 10 || value.length == 13 ? null : "Enter a valid ISBN";
+
   void isbnFormSubmit() {
     if (_formKey.currentState.validate()) {
-      print("Isbn");
+      print("Isbn: ${_isbnController.text}");
     } else {
       print("Invalid Isbn");
     }
