5a683a06dc4894522df2d9e069e37b4cc125590f
Nathan Button <nate601@live.com>
2019-06-20 04:01:02 -0500

    Add IsbnForm Template

--- lib/main.dart
+++ lib/main.dart
@@ -28,29 +28,63 @@ class RootPage extends StatelessWidget {
           "Rapere Librum",
         ),
       ),
-      body: Column(
+      body: Container(
+        child: IsbnForm(),
+      ),
+    );
+  }
+}
+
+class IsbnForm extends StatefulWidget {
+  const IsbnForm({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _IsbnFormState createState() => _IsbnFormState();
+}
+
+class _IsbnFormState extends State<IsbnForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
         children: <Widget>[
-          Container(
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "Isbn:",
-                      hintText: "Isbn 10/13",
-                      icon: Icon(Icons.book)),
-                  validator: (String value) {
-                    if (value.length != 10 || value.length != 13)
-                      return "Isbn must be a valid Isbn 10 or Isbn 13";
-                    else
-                      return null;
-                  },
-                  keyboardType: TextInputType.number,
-                ),
-              ],
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "ISBN:",
+                filled: true,
+                border: OutlineInputBorder(),
+                icon: Icon(Icons.book),
+              ),
+              keyboardType: TextInputType.number,
+              validator: (String value) =>
+                  value.length == 10 || value.length == 13
+                      ? null
+                      : "Enter a valid ISBN",
+              autovalidate: true,
             ),
           ),
+          RaisedButton(
+            child: Text("Search"),
+            onPressed: isbnFormSubmit,
+            padding: EdgeInsets.symmetric(horizontal: 100),
+          )
         ],
       ),
     );
+  }
+
+  void isbnFormSubmit() {
+    if (_formKey.currentState.validate()) {
+      print("Isbn");
+    } else {
+      print("Invalid Isbn");
+    }
   }
 }
