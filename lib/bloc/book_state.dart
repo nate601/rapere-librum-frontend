5de9c942def5031cd954bc9add065726faf2deab
Nathan Button <nate601@live.com>
2019-06-20 14:27:04 -0500

    Add block state scaffolding

--- lib/bloc/book_state.dart
+++ lib/bloc/book_state.dart
@@ -1,9 +1,23 @@
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
+import 'Model/BookDetails.dart';
+
 @immutable
 abstract class BookState extends Equatable {
   BookState([List props = const []]) : super(props);
 }
 
-class InitialBookState extends BookState {}
+class BookInitial extends BookState {}
+
+class BookLoadingDetails extends BookState {
+  final String isbn;
+
+  BookLoadingDetails(this.isbn) : super([isbn]);
+}
+
+class BookLoadedDetails extends BookState {
+  final BookDetails foundBook;
+
+  BookLoadedDetails(this.foundBook) : super([foundBook]);
+}
