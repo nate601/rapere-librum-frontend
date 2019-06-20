45b97a77cece23bb5d7f36232a3fab36588087f4
Nathan Button <nate601@live.com>
2019-06-19 22:32:34 -0500

    Began scaffolding for search root page

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,46 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "Rapere Librum",
+        home: new RootPage());
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Rapere Librum",
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Isbn:",
+                      hintText: "Isbn 10/13",
+                      icon: Icon(Icons.book)),
+                  validator: (String value) {
+                    if (value.length != 10 || value.length != 13)
+                      return "Isbn must be a valid Isbn 10 or Isbn 13";
+                    else
+                      return null;
+                  },
+                  keyboardType: TextInputType.number,
+                ),
+              ],
             ),
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
