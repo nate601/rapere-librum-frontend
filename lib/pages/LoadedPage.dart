bb3a96623bae7313d50a512396c1cdfbe9b7594a
Nathan Button <nate601@live.com>
2019-06-20 23:17:37 -0500

    Refactor GoogleInfoCard

--- lib/pages/LoadedPage.dart
+++ lib/pages/LoadedPage.dart
@@ -27,48 +27,15 @@ class _LoadedPageState extends State<LoadedPage> {
           onPressed: goBack,
         ),
       ),
-      body: buildCard(widget.details),
+      body: buildBody(widget.details),
     );
   }
 
-  Widget buildCard(BookDetails details) {
+  Widget buildBody(BookDetails details) {
     return Column(
       children: <Widget>[
-        Card(
-          child: Row(
-            children: <Widget>[
-              SizedBox(
-                child: new Image.network(details.thumbnailUrl),
-                width: 128,
-                height: 206,
-              ),
-              Padding(
-                padding: EdgeInsets.all(8),
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                verticalDirection: VerticalDirection.down,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Text(
-                    "${details.bookName}",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text(
-                    "${details.authorName}",
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                  Stars(
-                    star: details.currentRating,
-                  )
-                ],
-              )
-            ],
-          ),
+        new GoogleInfoCard(
+          details: details,
         ),
       ],
     );
@@ -78,5 +45,54 @@ class _LoadedPageState extends State<LoadedPage> {
     final bloc = BlocProvider.of<BookBloc>(context);
 
     bloc.dispatch(ClearSelection());
+  }
+}
+
+class GoogleInfoCard extends StatelessWidget {
+  const GoogleInfoCard({
+    Key key,
+    @required this.details,
+  }) : super(key: key);
+
+  final BookDetails details;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            child: new Image.network(details.thumbnailUrl),
+            width: 128,
+            height: 206,
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            verticalDirection: VerticalDirection.down,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(
+                "${details.bookName}",
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                "${details.authorName}",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              Stars(
+                star: details.currentRating,
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
